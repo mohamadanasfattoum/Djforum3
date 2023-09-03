397d4bcb7f471a45978cbb2134234e0b549f01f7
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-03 19:25:26 +0200

    add user question settings

--- question/models.py
+++ question/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Question(models,Model):
-    pass
+
+class Question(models.Model):
+    user = models.ForeignKey(User,related_name='question_user',on_delete=models.SET_NULL, null=True)
