cdab633ba65661f341d0cf73f324e84b9ff74a12
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-03 19:50:32 +0200

    add Europe/Berlin

--- project/settings.py
+++ project/settings.py
@@ -104,13 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'Europe/Berlin'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
