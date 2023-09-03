90826bace779135c70510a54de74cf652479a939
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-03 19:26:34 +0200

    add title question settings

--- question/models.py
+++ question/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     user = models.ForeignKey(User,related_name='question_user',on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=120)
