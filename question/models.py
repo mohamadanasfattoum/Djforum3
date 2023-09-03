998d8413053baf7c322d311852eeba41a002aba9
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-03 19:31:14 +0200

    add time question settings

--- question/models.py
+++ question/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 class Question(models.Model):
     user = models.ForeignKey(User,related_name='question_user',on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=120)
     content = models.TextField(max_length=5000)
-    
+    created_at = models.DateTimeField(default=timezone.now)
