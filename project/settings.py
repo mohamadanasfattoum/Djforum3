5b41266107182730a31655e8b3308faf5dd456ba
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-03 19:55:04 +0200

    add correct in Time Zone Europe/Berlin

--- project/settings.py
+++ project/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'Europe/Berlin'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
