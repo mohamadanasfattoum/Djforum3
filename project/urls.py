754bcfc7d99ba42cba2d44d3aa282ea455bec042
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-04 20:08:31 +0200

    add  question_listin urls

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from question.views import question_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('question/',question_list),
 ]
